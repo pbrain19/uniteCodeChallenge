a04437d740d0b142ce768e6e271e984b7c2769ec
pbrain19 <pbrain19@gmail.com>
2016-05-01 22:30:48 -0400

    controller setup. Alerts added.

--- controller.js
+++ controller.js
@@ -1,5 +1,19 @@
 var app = angular.module('uniteUs');
 
-app.controller('assistanceFormCTRL', function($scope) {
-  $scope.serviceRequest = {};
+app.controller('assistanceFormCTRL', function($scope, Assistance) {
+
+  $scope.createRequest = function(assistanceDetails) {
+    Assistance.create()
+      .then(function(message) {
+        $scope.alerts = [{type: 'success', msg: message}];
+      })
+      .catch(function(errorMessage) {
+        $scope.alerts = [{type: 'danger', msg: errorMessage}];
+      });
+  }
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+
 });
